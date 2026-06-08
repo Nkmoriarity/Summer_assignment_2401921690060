c131be8a07aaaf5686b2691a5fd184e729f54824
Shivam Kumar <sk7970459@gmail.com>
2026-06-08 23:26:22 +0530

    Implement first unique character finder in string

--- Week_2/Day_6/First Unique Character in a String.cpp
+++ Week_2/Day_6/First Unique Character in a String.cpp
@@ -1,1 +1,16 @@
-
+//                 First Unique Character in a String
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int a[26]={0};
+        for(int i=0;i<s.size();i++){
+            a[s[i]-'a']++;
+        }
+        for(int j=0;j<s.size();j++){
+            if(a[s[j]-'a']==1){
+                return j;
+            }
+        }
+        return -1;
+    }
+};

0800fbd3fa64e02badbe8d7472f028c09f542861
Shivam Kumar <sk7970459@gmail.com>
2026-06-28 05:27:59 +0530

    Add palindrome linked list solution
    Implemented a solution to check if a singly-linked list is a palindrome using a stack.

---
+++ Week_3/DAY_2/234. Palindrome Linked List.cpp
@@ -0,0 +1,33 @@
+//                             234. Palindrome Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* temp=head;
+        stack<int>s;
+        while(temp!=NULL){
+            s.push(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            if(s.top()!=temp->val){
+                return false;
+            }
+            else{
+                s.pop();
+                temp=temp->next;
+            }
+        }
+        return true;
+    }
+};

a4284095604d2c5676a8c3db64e77525de7faa36
Shivam Kumar <sk7970459@gmail.com>
2026-06-28 05:05:49 +0530

    Add Reverse Linked List implementation
    Implemented the reverseList function to reverse a singly linked list.

---
+++ Week_3/Day_1/Reverse Linked List.cpp
@@ -0,0 +1,25 @@
+//                         206. Reverse Linked List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp=head;
+        ListNode* ans=NULL;
+        while(temp!=NULL){
+            temp=temp->next;
+            head->next=ans;
+            ans=head;
+            head=temp;
+        }
+        return ans;
+    }
+};

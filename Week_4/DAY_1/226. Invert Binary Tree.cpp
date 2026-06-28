36f847c9918d4e237604f987372f649689a1149a
Shivam Kumar <sk7970459@gmail.com>
2026-06-28 05:51:51 +0530

    Implement invertTree function to invert a binary tree

---
+++ Week_4/DAY_1/226. Invert Binary Tree.cpp
@@ -0,0 +1,13 @@
+//                        226. Invert Binary Tree
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root){
+            return nullptr;
+        }
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
